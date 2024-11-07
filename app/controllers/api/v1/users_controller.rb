4b268d97dedc2ede1af13b72e3076a3392ab701f
Erin Pintozzi <epintozzi@turing.edu>
2024-11-07 17:13:16 -0600

    reformat users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,20 +1,24 @@
-class Api::V1::UsersController < ApplicationController
-  def create
-    user = User.new(user_params)
-    if user.save
-      render json: UserSerializer.new(user), status: :created
-    else
-      render json: ErrorSerializer.format_error(ErrorMessage.new(user.errors.full_messages.to_sentence, 400)), status: :bad_request
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def create
+        user = User.new(user_params)
+        if user.save
+          render json: UserSerializer.new(user), status: :created
+        else
+          render json: ErrorSerializer.format_error(ErrorMessage.new(user.errors.full_messages.to_sentence, 400)), status: :bad_request
+        end
+      end
+
+      def index
+        render json: UserSerializer.format_user_list(User.all)
+      end
+
+      private
+
+      def user_params
+        params.permit(:name, :email, :password, :password_confirmation)
+      end
     end
-  end
-
-  def index
-    render json: UserSerializer.format_user_list(User.all)
-  end
-
-  private
-
-  def user_params
-    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
