797a01a2814674d488ffc607de00b9392833b042
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-12-03 16:27:02 -0500

    Added validations to comapies attributes and tested with shoulda matchers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :companies
+  has_many :companies, dependent: :destroy
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: { require: true }
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Company, type: :model do
   describe "associations" do
     it { should belong_to(:user) }
   end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:website) }
+    it { should validate_presence_of(:street_address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip_code) }
+    it { should validate_presence_of(:notes) }
+  end
 end
