685ea973aeba8634d880adeede9780e87b59df77
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-12-03 16:03:28 -0500

    Migrated companies table and setting up one to many associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :companies
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: { require: true }
