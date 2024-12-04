9faaa890846135259291d05bef4ad50769fd15d6
Candice Cirbo <thecirbos@icloud.com>
2024-12-03 17:16:52 -0700

    Feat: added users as f.key to contacts
    Co-authored-by: Renee rmessersmith25@gmail.com

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_03_224311) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_04_000148) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_03_224311) do
     t.text "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_03_224311) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "contacts", "users"
 end
