447b87c517919ae85fef3389c94d827e2a55b310
Candice Cirbo <thecirbos@icloud.com>
2024-12-03 17:00:14 -0700

    Feat: adds contact model, spec and schema update
    Co-authored-by: Renee rmessersmith25@gmail.com

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_07_221005) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_03_224311) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "company"
+    t.string "email"
+    t.string "phone_number"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Contact, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should have_secure_password }
+  end
+end
