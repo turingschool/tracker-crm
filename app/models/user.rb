c3fe2e05a9f15f3bc113ce151fd544e4219566be
Candice Cirbo <thecirbos@icloud.com>
2024-12-03 17:22:22 -0700

    Feat/test: added relationship to user and tested it
    Co-authored-by: Renee rmessersmith25@gmail.com

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password, presence: { require: true }
   has_secure_password
+
+  has_many :contacts, dependent: :destroy
 end
