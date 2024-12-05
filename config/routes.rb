3189d75505f614382638d1ef45ab9ce286e611c3
Shane Galvin <Sgalvin36@gmail.com>
2024-12-05 10:02:40 -0700

    Added tests for update function on user controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create, :index, :show]
+      resources :users, only: [:create, :index, :show, :update]
       resources :sessions, only: :create
     end
   end
