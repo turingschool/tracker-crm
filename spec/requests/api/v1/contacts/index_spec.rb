0fd63aac633b2445e4e43375f72d187092bb2eec
Renee Messersmith <morganrmessersmith@gmail.com>
2024-12-04 21:21:49 -0500

    Test: happy path testing for returning a serialized list of contacts for a user

---
+++ spec/requests/api/v1/contacts/index_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+describe "Contacts Controller", type: :request do
+  describe "#index action" do
+    context "returns a list of all contacts for a specific user" do
+      before(:each) do
+        @user = User.create(name: "Me", email: "its_me", password: "reallyGoodPass")
+        Contact.create!(first_name: "John", last_name: "Smith", company: "Turing", email: "123@example.com", phone_number: "(123) 555-6789", notes: "Notes here...", user_id: @user.id)
+
+        user_params = { email: "its_me", password: "reallyGoodPass" }
+        post api_v1_sessions_path, params: user_params, as: :json
+        @token = JSON.parse(response.body)["token"]
+      end
+
+      it "should return 200 and provide the appropriate contacts" do
+        get api_v1_contacts_path, headers: { "Authorization" => "Bearer #{@token}" }, as: :json
+
+        expect(response).to be_successful
+        json = JSON.parse(response.body, symbolize_names: true)[:data]
+   
+        expect(json.first[:attributes]).to include(:first_name, :last_name, :company, :email, :phone_number, :notes, :user_id)
+      end
+    end
+
+    context "request is invalid" do
+      it "should return error for bad user ID" do
+     
+      end
+    end
+  end
+end
