1d5005281c3ca6a4e5c26319b3e975f1a622c77b
Jim Macur <jim.macur@gmail.com>
2024-12-05 13:13:14 -0700

    refactor: moves authenticate user to application controller

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -33,19 +33,6 @@ module Api
         params.permit(:name, :website, :street_address, :city, :state, :zip_code, :notes)
       end
 
-      def authenticate_user
-        token = request.headers['Authorization']&.split(' ')&.last
-        if token
-          begin
-            payload = decoded_token(token)
-            @current_user = User.find_by(id: payload[:user_id])
-          rescue JWT::DecodeError
-            @current_user = nil
-          end
-        end
-        render json: { error: 'Not authenticated' }, status: :unauthorized unless @current_user
-      end
-
       def decoded_token(token)
         JWT.decode(token, Rails.application.secret_key_base, true, { algorithm: 'HS256' })[0].symbolize_keys
       end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::API
- 
+
+  private
+
+  def authenticate_user
+    token = request.headers['Authorization']&.split(' ')&.last
+    if token
+      begin
+        payload = decoded_token(token)
+        @current_user = User.find_by(id: payload[:user_id])
+      rescue JWT::DecodeError
+        @current_user = nil
+      end
+    end
+    render json: { error: 'Not authenticated' }, status: :unauthorized unless @current_user
+  end
+
+
 end
