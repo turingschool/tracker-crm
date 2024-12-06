19068776ba90ea151712be49343c34698988472b
Stefan Bloom <stefanjbloom88@gmail.com>
2024-12-06 16:07:47 -0700

    Docs: pseudocode for future readme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   include Pundit::Authorization
   after_action :verify_authorized
 
-  # temporary current user testing stub until we add in authentication
+  # temporary current_user testing stub until we add in authentication
   def current_user
     @current_user ||= User.find_by(email: "test@test.com")
   end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,33 +1,35 @@
 class UserPolicy < ApplicationPolicy
 # Feel free to use this as a template for your own policies
-# that will be used by their corresponding controller
+# that you'll define and then will be used by their corresponding controller.
 
   def create?
     true
   end
-  # anyone site visitor can create a user
+  # anyone site visitor can #create a user
 
   def index?
     admin?
   end
-  # only an admin can view all the users
+  # only an admin can view all the users #index
 
   def show?
     admin? || user == record
   end
-  # admin can see any user, a user can only see their own record/themselves
+  # admin can see any user, a user can only see their own record/themselves #show
 
   def update?
     admin? || user == record
   end
-  # admin can update any user, a user can only update their own record/themselves
+  # admin can #update any user, a user can only #update their own record/themselves
 
   class Scope < ApplicationPolicy::Scope
     # NOTE: Be explicit about which records you allow access to!
-    def resolve
-      if admin?
+    # Scope nested class defines which records a user is authorized to access when retrieving a collection of objects, such as in an index action.
+
+    def resolve #resolve determines which records a user is allowed to access 
+      if admin? #method inherited from ApplicationPolicy to check role of current user(:admin)
         scope.all
-      elsif user?
+      elsif user?#method inherited from ApplicationPolicy to check role of current user(:user)
         scope.where(id: user.id)
       else
         scope.none
