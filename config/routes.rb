af16496c20f87c633aa95fb0fbd8a13bcbc1bf03
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-12-06 13:45:00 -0700

    Fix: restore update action to routes after losing in merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create, :index, :show] do
+      resources :users, only: [:create, :index, :show, :update] do
         resources :job_applications, only: :create
       end
       resources :companies, only: [:create, :index]
