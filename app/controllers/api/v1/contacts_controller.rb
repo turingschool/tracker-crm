5abaf0f38ea7bcaa11193c2367c8524439b26156
Candice Cirbo <thecirbos@icloud.com>
2024-12-06 19:39:42 -0700

    Feat: create action to contacts controller

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -12,6 +12,20 @@ module Api
         end
       end
 
-    end
+			def create 
+				contact = @current_user.contacts.new(contact_params)
+				if contact.save
+					render json: ContactsSerializer.new(contact), status: :created
+				else
+					render json: { error: contact.errors.full_messages.to_sentence }, status: :unprocessable_entity
+				end
+			end
+
+			private
+
+			def contact_params
+				params.require(:contact).permit(:first_name, :last_name, :company_id, :email, :phone_number, :notes)
+			end
+	  end
   end
 end
