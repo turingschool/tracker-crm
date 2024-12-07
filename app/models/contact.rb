e697ae38f014af72fe414bf9b6ed378b278e16c5
Candice Cirbo <thecirbos@icloud.com>
2024-12-06 20:49:13 -0700

    Feat: adds uniqueness validation model/cleans up minor formatting

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,7 @@
 class Contact < ApplicationRecord
-  validates :first_name, :last_name, presence: true
-  
   belongs_to :user
+  
+  validates :first_name, presence: {message: "can't be blank"} 
+  validates :last_name, presence: {message: "can't be blank"}
+  validates :first_name, uniqueness: { scope: [:last_name, :user_id], message: "and Last name already exist for this user" }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   describe "associations" do
     it { should have_many(:companies) }
   end
+  
   describe "validations" do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
