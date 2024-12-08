2bb527453eba6c451b77634a5f7db90a8cb3cc77
Kyle Delaney <khdelaney99@gmail.com>
2024-12-08 15:41:30 -0500

    Refactor: add validation for application url to job_application model

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -6,7 +6,7 @@ class JobApplication < ApplicationRecord
   validates :date_applied, presence: true
   validates :status, presence: true
   validates :job_description, presence: true
-  validates :application_url, presence: true
+  validates :application_url, presence: true, uniqueness: { scope: :user_id, message: "You already have an application with this URL" }
   validates :contact_information, presence: true
   validates :company_id, presence: true
 end
