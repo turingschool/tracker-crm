bcbf3918d91f29c22d099ab58097eab37b902c78
Stefan Bloom <stefanjbloom88@gmail.com>
2024-12-10 19:30:04 -0700

    Feat: dashboard serializer built to use relationships to render nested JSON attributes

--- app/controllers/api/v1/dashboards_controller.rb
+++ app/controllers/api/v1/dashboards_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::DashboardsController < ApplicationController
+
+  def show
+    user = User.find(params[:id])
+    authorize user
+    render json: DashboardSerializer.new(user)
+  end
 end
--- app/serializers/dashboard_serializer.rb
+++ app/serializers/dashboard_serializer.rb
@@ -1,4 +1,18 @@
 class DashboardSerializer
   include JSONAPI::Serializer
-  attributes 
+  attributes :id, :name, :email
+
+  has_many :job_applications, serializer: JobApplicationSerializer
+  has_many :contacts, serializer: ContactsSerializer
+  has_many :companies, serializer: CompanySerializer
+
+  attribute :dashboard do |user|
+    {
+      weekly_summary: {
+        job_applications: user.job_applications,
+        new_contacts: user.contacts,
+        companies: user.companies
+      }
+    }
+  end
 end
