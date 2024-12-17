997d1e0f347d47a0d99e3da551f3f2743d6c7379
Candice Cirbo <thecirbos@icloud.com>
2024-12-17 16:19:04 -0700

    Feat: updated contact controller and serializer

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -4,6 +4,17 @@ module Api
       before_action :authenticate_user
 
       def index
+        if params[:company_id]
+        company = @current_user.companies.find_by(id: params[:id])
+        authorize company
+  
+        if company
+          contacts = company.contacts
+          render json: { company: CompanySerializer.new(company), contacts: ContactSerializer.new(contacts) }
+        else
+          render json: { error: "Company not found or unauthorized access" }, status: :not_found
+        end
+      else
 				authorize Contact
         contacts = @current_user.contacts
         if contacts.empty?
@@ -12,10 +23,31 @@ module Api
           render json: ContactsSerializer.new(contacts), status: :ok
         end
       end
+    end
+
+    # def show
+    #   contact = @current_user.contacts.find_by(id: params[:id])
+    #   authorize contact
+    
+    #   if contact
+    #     render json: ContactsSerializer.new(contact), status: :ok
+    #   else
+    #     render json: { error: "Contact not found or unauthorized access" }, status: :not_found
+    #   end
+    # end
 
 			def create 
 				authorize Contact
+        if params[:company_id] 
+          company = @current_user.companies.find_by(id: params[:company_id])
+          if company
+            contact = company.contacts.new(contact_params.merge(user_id: @current_user.id))
+          else
+            return render json: { error: "Company not found" }, status: :not_found
+          end
+        else
 				contact = @current_user.contacts.new(contact_params)
+        end
 				if contact.save
 					render json: ContactsSerializer.new(contact), status: :created
 				else
--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,10 @@ Rails.application.routes.draw do
       resources :users, only: [:create, :index, :show, :update] do
 
         resources :job_applications, only: [:create, :index, :show]
-        resources :companies, only: [:create, :index]
-        resources :contacts, only: [:create, :index]
+        resources :companies, only: [:create, :index] do
+          resources :contacts, only: [:create, :index]
+        end
+        resources :contacts, only: [:index, :create]
         resource :dashboard, only: :show
       end
 
@@ -21,3 +23,4 @@ Rails.application.routes.draw do
     end
   end
 end
+
