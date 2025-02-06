c3656ce66840ae6f28a208a70414cb997e879848
James-Cochran <jameslikesfruit@gmail.com>
2025-02-06 18:08:57 -0500

    Feat: add handle_deletion method to Company model, set dependent: :destroy on job_applications

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,7 +2,12 @@ class Company < ApplicationRecord
   rolify strict: true
   belongs_to :user 
   has_many :contacts
-  has_many :job_applications
+  has_many :job_applications, dependent: :destroy
   
   validates :name, presence: true
+
+  def handle_deletion
+    contacts.update_all(company_id: nil)
+    destroy
+  end
 end
