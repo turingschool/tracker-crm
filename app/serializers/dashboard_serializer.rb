1313e6050c4884d6a9108509ca29c0a1fb08ee99
Alora Riley <alora.riley@gmail.com>
2025-02-14 15:51:43 -0600

    feat: Use Active Record to filter data that is sent to the dashboard.

--- app/serializers/dashboard_serializer.rb
+++ app/serializers/dashboard_serializer.rb
@@ -5,9 +5,9 @@ class DashboardSerializer
   attribute :dashboard do |user|
     {
       weekly_summary: {
-        job_applications: user.job_applications,
-        contacts: user.contacts,
-        companies: user.companies
+        job_applications: user.job_applications.where('created_at >= ?', 7.days.ago),
+        contacts: user.contacts.where('created_at >= ?', 7.days.ago),
+        companies: user.companies.where('created_at >= ?', 7.days.ago)
       }
     }
   end
