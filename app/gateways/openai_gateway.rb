bad26a5c5970a7619d52bef4a1f47c2a5772f271
Wally Wallace <jrvw001@gmail.com>
2025-02-21 13:10:01 -0700

    Create skeleton gateway and gateway spec files

---
+++ app/gateways/openai_gateway.rb
@@ -0,0 +1,11 @@
+class OpenaiGateway
+  def generate_interview_questions(params)
+    prompt = build_prompt(params)
+  end
+
+  private
+
+  def build_prompt(params)
+    #create the prompt here
+  end
+end
---
+++ spec/gateways/openai_gateway_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe OpenaiGateway do
+  # successful request
+  # unsuccessful request
+  # build prompt happy path
+  # build prompt sad path
+end
