0395f4881789cf8d455d75436e845f471ffc6ed6
Kaelin Salazar <kaelinpsalazar@gmail.com>
2025-02-26 08:05:37 -0700

    had to add the scope class to my policy file for user authentication

--- app/policies/interview_question_policy.rb
+++ app/policies/interview_question_policy.rb
@@ -2,4 +2,14 @@ class InterviewQuestionPolicy < ApplicationPolicy
   def index?
     admin? || user.present?
   end
+
+  class Scope < Scope
+    def resolve
+      if admin?
+        scope.all
+      else
+        scope.where(user: user)
+      end
+    end
+  end
 end
