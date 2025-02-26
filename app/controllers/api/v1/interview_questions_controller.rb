968b142bbfc09a5b4409dc49911c68be453ff246
Kaelin Salazar <kaelinpsalazar@gmail.com>
2025-02-26 08:11:09 -0700

    impliment serializer in the interview question controller

--- app/controllers/api/v1/interview_questions_controller.rb
+++ app/controllers/api/v1/interview_questions_controller.rb
@@ -2,8 +2,16 @@ class Api::V1::InterviewQuestionsController < ApplicationController
   before_action :authenticate_user
 
   def index
-    interview_questions = InterviewQuestion.all
-    interview_questions.each { |question| authorize question } 
-    render json: interview_questions
+    interview_questions = policy_scope(InterviewQuestion) 
+    authorize interview_questions
+    render json: interview_questions, each_serializer: InterviewQuestionSerializer
+  end
+
+  private
+
+  def interview_question_params
+    params.require(:interview_question).permit(:question, :user_id)
+  rescue ActionController::ParameterMissing
+    nil
   end
 end
