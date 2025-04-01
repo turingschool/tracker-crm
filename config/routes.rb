90a4123de59ed308dd87ca0ac6fa547e5c24082e
Kristin Weiland <kristinweiland@gmail.com>
2025-03-31 18:46:47 -0700

    Correct route name for interview questions.

--- config/routes.rb
+++ config/routes.rb
@@ -11,18 +11,21 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create, :index, :show, :update] do
 
-        resources :job_applications, only: [:create, :index, :show, :update, :destroy]
-        resources :companies, only: [:create, :index, :update, :destroy] do
+        resources :job_applications, only: [:create, :index, :show, :update, :destroy] do
+          get 'interview_questions', to: 'interview_questions#show'
+        end
 
+        resources :companies, only: [:create, :index, :update, :destroy] do
           resources :contacts, only: [:create, :index]
         end
+
         resources :contacts, only: [:index, :create, :show, :destroy, :update]
         resource :dashboard, only: :show
       end
 
       resources :sessions, only: :create
       resources :interview_questions, only: [:index]
-      post 'interview_questions', to: 'interview_questions#index'
+      # post 'interview_questions', to: 'interview_questions#index'
     end
   end
 end
