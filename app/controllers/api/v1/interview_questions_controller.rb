4a1d38bb95b4b0a278899c9935ac97fbfe33b6de
Kristin Weiland <kristinweiland@gmail.com>
2025-04-01 21:56:51 -0700

    Refactor controller to user create action instead of show. Utilizes service objec too.

--- app/controllers/api/v1/interview_questions_controller.rb
+++ app/controllers/api/v1/interview_questions_controller.rb
@@ -1,49 +1,76 @@
 class Api::V1::InterviewQuestionsController < ApplicationController
   before_action :authenticate_user
+  before_action :set_job_application
   # require_relative '../../../gateways/openai_gateway'
 
-  def show
-    job_application_id = params[:job_application_id]
 
-    job_application = JobApplication.find_by(id: job_application_id)
+  def create
+    authorize @job_application, :show?
 
-    unless job_application
-      skip_authorization
-      render json: { error: "Job application not found" }, status: :not_found
-      return
-    end
+    result = InterviewQuestionGeneratorService.call(@job_application)
 
-    authorize job_application, :show?
-    
-    existing_interview_questions = InterviewQuestion.where(job_application_id: job_application_id)
-
-    if existing_interview_questions.present?
-      formatted_questions = {
-        id: "existing-questions-for-#{job_application_id}",
-        data: existing_interview_questions.map.with_index(1) do | question, index |
-          {
-            index: index,
-            type: "interview_question",
-            attributes: {
-              question: question.question
-            }
-          }
-        end
-      }
-      return render json: formatted_questions, status: :ok 
-    end
-
-    job_description = job_application.job_description
-    ai_response = OpenaiGateway.new.generate_interview_questions(job_description)
-
-    if ai_response[:success]
-      render json: InterviewQuestionSerializer.format_questions(ai_response[:data], ai_response[:id]), status: 200
+    if result[:success]
+      render json: result[:data], status: :created
     else
-      error_message = ErrorMessage.new("Failed to generate interview questions.", 400)
-      render json: ErrorSerializer.format_error(error_message), status: :bad_request
-      return
+      render json: ErrorSerializer.format_error(result[:error]), status: :bad_request
     end
   end
+
+
+  private
+
+  def set_job_application
+    @job_application = JobApplication.find_by(id: params[:job_application_id])
+    return if @job_application
+      skip_authorization
+    render json: { error: "Job application not found" }, status: :not_found
+  end
+
+end
+
+
+
+  # def show
+  #   # job_application_id = params[:job_application_id]
+
+  #   # job_application = JobApplication.find_by(id: job_application_id)   
+
+  #   authorize @job_application, :show?
+    
+  #   existing_interview_questions = InterviewQuestion.where(job_application_id: params[:job_application_id])
+
+  #   if existing_interview_questions.present?
+  #     formatted_questions = {
+  #       id: "existing-questions-for-#{@job_application.id}",
+  #       data: existing_interview_questions.map.with_index(1) do | question, index |
+  #         {
+  #           index: index,
+  #           type: "interview_question",
+  #           attributes: {
+  #             question: question.question
+  #           }
+  #         }
+  #       end
+  #     }
+  #     return render json: formatted_questions, status: :ok 
+  #   end
+
+  #   job_description = @job_application.job_description
+  #   ai_response = OpenaiGateway.new.generate_interview_questions(job_description)
+
+  #   if ai_response[:success]
+  #     render json: InterviewQuestionSerializer.format_questions(ai_response[:data], ai_response[:id]), status: 200
+  #   else
+  #     error_message = ErrorMessage.new("Failed to generate interview questions.", 400)
+  #     render json: ErrorSerializer.format_error(error_message), status: :bad_request
+  #     return
+  #   end
+  # end
+
+
+
+
+
 
   # def index
   #   authorize InterviewQuestion
@@ -72,4 +99,4 @@ class Api::V1::InterviewQuestionsController < ApplicationController
   # rescue ActionController::ParameterMissing
   #   nil
   # end
-end
+
