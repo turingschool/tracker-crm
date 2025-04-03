8e382b6ef3b30e16d0008d6348e42c25eb1d3a70
Kristin Weiland <kristinweiland@gmail.com>
2025-04-03 10:09:44 -0700

    Update controller to utilize a custom method: fetch_or_create to clearly communicates the intent as standard CRUD actions didn't quite fit.

--- app/controllers/api/v1/interview_questions_controller.rb
+++ app/controllers/api/v1/interview_questions_controller.rb
@@ -2,11 +2,12 @@ class Api::V1::InterviewQuestionsController < ApplicationController
   before_action :authenticate_user
   before_action :set_job_application
 
-  def create
+
+  def fetch_or_create
     authorize @job_application, :show?
-
+  
     result = InterviewQuestionGeneratorService.call(@job_application)
-
+  
     if result[:success]
       render json: result[:data], status: :ok
     else
@@ -14,14 +15,14 @@ class Api::V1::InterviewQuestionsController < ApplicationController
     end
   end
 
-
   private
 
   def set_job_application
     @job_application = JobApplication.find_by(id: params[:job_application_id])
-    return if @job_application
+    unless @job_application
       skip_authorization
-    render json: { error: "Job application not found" }, status: :not_found
+      render json: { error: "Job application not found" }, status: :not_found
+      return
+    end
   end
-
 end
