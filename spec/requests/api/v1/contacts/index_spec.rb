5b3e7669636bef306f07308d114c764703d2b36b
Joel Davalos <jsdavalos@gmail.com>
2025-04-09 17:47:35 -0600

    Refactor/updated index action tests to incorporate factorybot and faker

--- spec/requests/api/v1/contacts/index_spec.rb
+++ spec/requests/api/v1/contacts/index_spec.rb
@@ -4,15 +4,15 @@ describe "Contacts Controller", type: :request do
   describe "#index action" do
     context "Happy Paths" do
       before(:each) do
-        @user = User.create!(name: "Me", email: "its_me", password: "reallyGoodPass")
-        @company = Company.create!(name: "Turing", website: "www.turing.com", street_address: "123 Main St", city: "Denver", state: "CO", zip_code: "80218", user_id: @user.id)
-        Contact.create!(first_name: "John", last_name: "Smith", company_id: @company.id, email: "123@example.com", phone_number: "123-555-6789", notes: "Notes here...", user_id: @user.id)
-        user_params = { email: "its_me", password: "reallyGoodPass" }
+        @user = create(:user)
+        @company = create(:company, user: @user)
+        @contact = create(:contact, company: @company, user: @user)
+        user_params = { email: @user.email, password: @user.password }
         post api_v1_sessions_path, params: user_params, as: :json
         @token = JSON.parse(response.body)["token"]
         
-        @user2 = User.create!(name: "Jane", email: "email", password: "Password")
-        user_params2 = { email: "email", password: "Password" }
+        @user2 = create(:user)
+        user_params2 = { email: @user2.email, password: @user2.password }
         post api_v1_sessions_path, params: user_params2, as: :json
         @token2 = JSON.parse(response.body)["token"]
       end
@@ -23,11 +23,11 @@ describe "Contacts Controller", type: :request do
         expect(response).to be_successful
         json = JSON.parse(response.body, symbolize_names: true)[:data].first
 
-        expect(json[:attributes][:first_name]).to eq("John")
-        expect(json[:attributes][:last_name]).to eq("Smith")
-        expect(json[:attributes][:email]).to eq("123@example.com")
-        expect(json[:attributes][:phone_number]).to eq("123-555-6789")
-        expect(json[:attributes][:notes]).to eq("Notes here...")
+        expect(json[:attributes][:first_name]).to eq(@contact.first_name)
+        expect(json[:attributes][:last_name]).to eq(@contact.last_name)
+        expect(json[:attributes][:email]).to eq(@contact.email)
+        expect(json[:attributes][:phone_number]).to eq(@contact.phone_number)
+        expect(json[:attributes][:notes]).to eq(@contact.notes)
         expect(json[:attributes][:company_id]).to eq(@company.id)
       end
 
@@ -36,7 +36,7 @@ describe "Contacts Controller", type: :request do
 
         expect(response).to be_successful
         json = JSON.parse(response.body, symbolize_names: true)
- 
+
         expect(json[:data]).to eq([])
         expect(json[:message]).to eq("No contacts found")
       end
@@ -48,17 +48,17 @@ describe "Contacts Controller", type: :request do
         expect(response).to have_http_status(:ok)
         json = JSON.parse(response.body, symbolize_names: true)
 
-        expect(json[:company][:data][:attributes][:name]).to eq("Turing")
-        expect(json[:contacts][:data][0][:attributes][:last_name]).to eq("Smith")
+        expect(json[:company][:data][:attributes][:name]).to eq(@company.name)
+        expect(json[:contacts][:data][0][:attributes][:last_name]).to eq(@contact.last_name)
 
       end
     end
 
     context "Sad Paths" do 
       before(:each) do
-        @user = User.create!(name: "Me", email: "its_me", password: "reallyGoodPass")
-        @company = Company.create!(name: "Turing", website: "www.turing.com", street_address: "123 Main St", city: "Denver", state: "CO", zip_code: "80218", user_id: @user.id)
-        user_params = { email: "its_me", password: "reallyGoodPass" }
+        @user = create(:user)
+        @company = create(:company, user: @user)
+        user_params = { email: @user.email, password: @user.password }
         post api_v1_sessions_path, params: user_params, as: :json
         @token = JSON.parse(response.body)["token"]
       end
@@ -82,10 +82,10 @@ describe "Contacts Controller", type: :request do
       end
   
       it "returns a 403 and an error message if the token is expired" do
-        user = User.create!(name: "Me", email: "email", password: "reallyGoodPass")
-        expired_token = JWT.encode({ user_id: user.id, exp: 1.hour.ago.to_i }, Rails.application.secret_key_base, "HS256")
+        @user3 = create(:user)
+        expired_token = JWT.encode({ user_id: @user3.id, exp: 1.hour.ago.to_i }, Rails.application.secret_key_base, "HS256")
   
-        get api_v1_user_contacts_path(user.id), headers: { "Authorization" => "Bearer #{expired_token}" }, as: :json
+        get api_v1_user_contacts_path(@user3.id), headers: { "Authorization" => "Bearer #{expired_token}" }, as: :json
   
         expect(response).to have_http_status(:unauthorized)
         json = JSON.parse(response.body, symbolize_names: true)
