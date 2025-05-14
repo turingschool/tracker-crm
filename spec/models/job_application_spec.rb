975415e3ffe7d50a2cf9e68c663e707cad8a4f48
Beverly Green <greenbev000@gmail.com>
2025-05-14 16:01:14 -0500

    test that job application is invalid when missing date when status is anything besides NOT applied yet

--- spec/models/job_application_spec.rb
+++ spec/models/job_application_spec.rb
@@ -1,6 +1,13 @@
 require "rails_helper"
 
 RSpec.describe JobApplication, type: :model do
+  describe "associations" do
+    it { should belong_to(:company) }
+    it { should belong_to(:user) }
+    it { should belong_to(:contact).optional }
+    it { should have_many(:interview_questions)}
+  end
+
   describe "validations" do
     it { should validate_presence_of(:position_title) }
     it { should validate_presence_of(:date_applied) }
@@ -77,12 +84,29 @@ RSpec.describe JobApplication, type: :model do
 
       expect(job_application_with_contact).to be_valid
     end
-  end
 
-  describe "associations" do
-    it { should belong_to(:company) }
-    it { should belong_to(:user) }
-    it { should belong_to(:contact).optional }
-    it { should have_many(:interview_questions)}
+    context 'when status is NOT "not_yet_applied"' do
+      it 'requires date_applied' do
+        contact = Contact.create!(
+        first_name: "John",
+        last_name: "Doe",
+        email: "john.doe@example.com",
+        user_id: subject.user_id
+      )
+
+      job_application = JobApplication.new(
+        position_title: "Full Stack Engineer",
+        status: :submitted,
+        job_description: "Wears many hats",
+        application_url: "http://example.com",
+        company_id: subject.company_id,
+        user_id: subject.user_id,
+        contact_id: contact.id
+      )
+
+        expect(job_application).to be_invalid
+        expect(job_application.errors[:date_applied]).to eq(["can't be blank"])
+      end
+    end
   end
 end
