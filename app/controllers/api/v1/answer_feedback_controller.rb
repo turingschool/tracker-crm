599df5fe4b859f7516dd1dbbed5038479ac73c8f
Will Fox <william.macdonald.fox@gmail.com>
2025-05-21 18:02:30 -0400

    Feat: build #create action in AnswerFeedbackController

--- app/controllers/api/v1/answer_feedback_controller.rb
+++ app/controllers/api/v1/answer_feedback_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::AnswerFeedbackController < ApplicationController
   def create
-    authorize @job_application, :show?
+    result = AnswerFeedbackService.call(
+      interview_question_id: params[:interview_question_id],
+      answer: params[:answer]
+    )
+
+    if result[:success]
+      render json: AnswerFeedbackSerializer.new(result[:data]), status: :created
+    else
+      render json: { error: result[:error] }, status: :unprocessable_entity
+    end
   end
 end
-
