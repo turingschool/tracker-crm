7505af8fef947785d1aa1a71618f433d65e5648f
Will Fox <william.macdonald.fox@gmail.com>
2025-05-22 17:40:51 -0400

    Refactor: edit AnswerFeedbackController to authorize BEFORE calling the service

--- app/controllers/api/v1/answer_feedback_controller.rb
+++ app/controllers/api/v1/answer_feedback_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::AnswerFeedbackController < ApplicationController
   def create
+    interview_question = InterviewQuestion.find(params[:id])
+    authorize interview_question, :answer_feedback?
+    
     result = AnswerFeedbackService.call(
       interview_question_id: params[:id],
       answer: params[:answer]
     )
 
     if result[:success]
+      
       render json: AnswerFeedbackSerializer.new(result[:data]), status: :created
     else
       render json: { error: result[:error] }, status: :unprocessable_entity
