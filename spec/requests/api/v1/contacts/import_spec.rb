afe4c9458d25f48149555747145e391d65ff73bf
Jacob Inman <jrinman6295@gmail.com>
2025-05-23 14:45:04 -0700

    spec fixed for csv parse on fe instead of be

--- spec/requests/api/v1/contacts/import_spec.rb
+++ spec/requests/api/v1/contacts/import_spec.rb
@@ -4,65 +4,109 @@ describe "ImportContactsController", type: :request do
   describe "create - Happy Paths" do
     before(:each) do
       @user = create(:user)
-      post apv_v1_sessions_path, params: { email: @user.email, password: @user.password }, as: :json
+      user_params = { email: @user.email, password: @user.password }
+      post apv_v1_sessions_path, params: user_params, as: :json
       @token = JSON.parse(response.body)["token"]
     end
 
     it "should return 201 and successfully imports multiple contacts" do
-      csv_to_import = {
-        file: fixture_file_upload("files/valid_contacts.csv", "text/csv") #these files still need to be created, I just want to ensure I test csv data
+      contacts_to_import = {
+        contacts: [
+          {
+            first_name: "Jacob",
+            last_name: "Sample Boy",
+            email: "sample@email.come",
+            phone_number: "555-555-5555",
+            notes: "Import trail blazer"
+          },
+          {
+            first_name: "Beverly",
+            last_name: "Also Help",
+            email: "helper@email.com",
+            phone_number: "123-456-7890",
+            notes: "Front End Help"
+          }
+        ]
       }
 
       post import_api_v1_user_contacts_path(@user.id),
-        params: csv_to_import,
-        headers: { "Authorization" => "Bearer #{@token}" }
+        params: contacts_to_import,
+        headers: { "Authorization" => "Bearer #{@token}" },
+        as: :json
 
       expect(response).to have_http_status(:created)
-      expect(Contact.count).to eq(2)
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json[:imported_count]).to eq(2)
+      expect(Contact.last.first_name).to eq("Beverly")
     end
 
     it "imports contacts successfully with only required fields, first name and last name, and returns a 201" do
-      csv_to_import = {
-        file: fixture_file_upload("files/only_required_fields_contacts.csv", "text/csv") #these files still need to be created, I just want to ensure I test csv data
+      contacts_to_import = {
+        contacts: [
+          {
+            first_name: "Jacob",
+            last_name: "Sample Boy"
+          },
+          {
+            first_name: "Beverly",
+            last_name: "Also Help"
+          }
+        ]
       }
 
       post import_api_v1_user_contacts_path(@user.id),
-        params: csv_to_import,
-        headers: { "Authorization" => "Bearer #{@token}" }
+        params: contacts_to_import,
+        headers: { "Authorization" => "Bearer #{@token}" },
+        as: :json
 
       expect(response).to have_http_status(:created)
-      expect(Contact.count).to eq(1)
-      expect(Contact.last.first_name).to be_present
-      expect(Contact.last.last_name).to be_present
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json[:imported_count]).to eq(1)
     end
   end
 
   describe "#import - Sad Paths" do
-    it "returns 422 if the CSV is missing required fields" do
-      csv_to_import = {
-          file: fixture_file_upload("files/invalid_missing_names.csv", "text/csv") #these files still need to be created, I just want to ensure I test csv data
-        }
+    it "returns 422 if there are missing required fields" do
+      contacts_to_import = {
+        contacts: [
+          {
+            first_name: "",
+            last_name: "Sample Boy",
+            email: "sample@email.come",
+            phone_number: "555-555-5555",
+            notes: "Import trail blazer"
+          },
+          {
+            first_name: "Beverly",
+            last_name: "",
+            email: "helper@email.com",
+            phone_number: "123-456-7890",
+            notes: "Front End Help"
+          }
+        ]
+      }
 
       post import_api_v1_user_contacts_path(@user.id),
-        params: csv_to_import,
-        headers: { "Authorization" => "Bearer #{@token}" }
+        params: contacts_to_import,
+        headers: { "Authorization" => "Bearer #{@token}" },
+        as: :json
 
-        
       expect(response).to have_http_status(:unprocessable_entity)
       json = JSON.parse(response.body, symbolize_names: true)
       expect(json[:message]).to include("Missing required fields")
     end
 
-    it "returns 422 if the CSV file is empty" do
-      csv_to_import = {
-          file: fixture_file_upload("files/empty.csv", "text/csv") #these files still need to be created, I just want to ensure I test csv data
-        }
+    it "returns 422 if the input is empty" do
+      contacts_to_import = {
+        contacts: []
+      }
 
       post import_api_v1_user_contacts_path(@user.id),
-        params: csv_to_import,
-        headers: { "Authorization" => "Bearer #{@token}" }
+        params: contacts_to_import,
+        headers: { "Authorization" => "Bearer #{@token}" },
+        as: :json
 
-        
       expect(response).to have_http_status(:unprocessable_entity)
       json = JSON.parse(response.body, symbolize_names: true)
       expect(json[:message]).to include("No contacts to import")
@@ -71,27 +115,62 @@ describe "ImportContactsController", type: :request do
 
   describe "#import - Edge Cases" do
     it "returns 401 when no auth token is provided" do
-      csv_to_import = {
-          file: fixture_file_upload("files/valid_contacts.csv", "text/csv") #these files still need to be created, I just want to ensure I test csv data
-        }
+      contacts_to_import = {
+        contacts: [
+          {
+            first_name: "Jacob",
+            last_name: "Sample Boy",
+            email: "sample@email.come",
+            phone_number: "555-555-5555",
+            notes: "Import trail blazer"
+          },
+          {
+            first_name: "Beverly",
+            last_name: "Also Help",
+            email: "helper@email.com",
+            phone_number: "123-456-7890",
+            notes: "Front End Help"
+          }
+        ]
+      }
 
-      post import_api_v1_user_contacts_path(@user.id), params: csv_to_import
+      post import_api_v1_user_contacts_path(@user.id),
+        params: contacts_to_import,
+        as: :json
 
       expect(response).to have_http_status(:unauthorized)
       json = JSON.parse(response.body, symbolize_names: true)
-      expect(json[:error]).to eq("Not authenticated")
+      expect(json[:error]).to eq("not authenticated")
     end
 
     it "returns a 401 for an invalid token" do
-      csv_to_import = {
-          file: fixture_file_upload("files/valid_contacts.csv", "text/csv") #these files still need to be created, I just want to ensure I test csv data
-        }
+      contacts_to_import = {
+        contacts: [
+          {
+            first_name: "Jacob",
+            last_name: "Sample Boy",
+            email: "sample@email.come",
+            phone_number: "555-555-5555",
+            notes: "Import trail blazer"
+          },
+          {
+            first_name: "Beverly",
+            last_name: "Also Help",
+            email: "helper@email.com",
+            phone_number: "123-456-7890",
+            notes: "Front End Help"
+          }
+        ]
+      }
 
-      post import_api_v1_user_contacts_path(@user.id), 
-        params: csv_to_import,
-        headers: { "Authorization" => "Bearer broken.token"}
+      post import_api_v1_user_contacts_path(@user.id),
+        params: contacts_to_import,
+        headers: { "Authorization" => "Bearer broken.token" },
+        as: :json
 
       expect(response).to have_http_status(:unauthorized)
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json[:error]).to eq("not authenticated")
     end
   end
 end
