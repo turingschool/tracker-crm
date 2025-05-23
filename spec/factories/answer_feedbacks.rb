3c492bf2c6e6a9c9aaee8b70f9a670e671778a00
Will Fox <william.macdonald.fox@gmail.com>
2025-05-22 21:01:15 -0400

    Feat: create answer_feedback factory

--- spec/factories/answer_feedbacks.rb
+++ spec/factories/answer_feedbacks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :answer_feedback do
-    interview_question { nil }
-    answer { "MyText" }
-    feedback { "MyText" }
+    answer { "In a project, I had to integrate an unfamiliar API..." }
+    feedback { "This shows initiative, but consider including a reflection on what you learned." }
+    interview_question
   end
 end
