61681597f1714384e1cf0d115857fc9880c23ff6
Will Fox <william.macdonald.fox@gmail.com>
2025-05-23 17:24:11 -0400

    Test: add sad path tests to AnswerFeedback #create spec, work in progress, not passing

--- spec/requests/api/v1/answer_feedback/create_spec.rb
+++ spec/requests/api/v1/answer_feedback/create_spec.rb
@@ -40,4 +40,52 @@ RSpec.describe "POST /api/v1/users/:user_id/job_applications/:job_application_id
     expect(json[:data][:attributes][:answer]).to eq(fake_feedback.answer)
     expect(json[:data][:attributes][:feedback]).to eq(fake_feedback.feedback)
   end
+
+  it "returns 401 when no token is provided (unauthenticated)" do
+    post "/api/v1/users/#{user.id}/job_applications/#{job_application.id}/interview_questions/#{interview_question.id}/answer_feedback",
+        params: valid_params,
+        headers: { "Content-Type" => "application/json", "Accept" => "application/json" }
+  
+    expect(response).to have_http_status(:unauthorized)
+  
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:error]).to eq("Not authenticated")
+  end
+
+  it "returns 422 when required params are missing" do
+    allow(AnswerFeedbackService).to receive(:call).and_return({
+      success: false,
+      error: "Answer can't be blank"
+    })
+  
+    post "/api/v1/users/#{user.id}/job_applications/#{job_application.id}/interview_questions/#{interview_question.id}/answer_feedback",
+        params: {}.to_json,
+        headers: headers
+  
+    expect(response).to have_http_status(:unprocessable_entity)
+  
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:error]).to eq("Answer can't be blank")
+  end
+
+  it "returns 403 when the user is authenticated but does not own the interview question" do
+    other_user = create(:user)
+    other_token = JWT.encode(
+      { user_id: other_user.id, roles: other_user.roles.pluck(:name), exp: 24.hours.from_now.to_i },
+      Rails.application.secret_key_base
+    )
+  
+    post "/api/v1/users/#{other_user.id}/job_applications/#{job_application.id}/interview_questions/#{interview_question.id}/answer_feedback",
+        params: valid_params,
+        headers: {
+          "Authorization" => "Bearer #{other_token}",
+          "Content-Type" => "application/json",
+          "Accept" => "application/json"
+        }
+  
+    expect(response).to have_http_status(:forbidden)
+  
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:error]).to eq("Not authorized")
+  end
 end
