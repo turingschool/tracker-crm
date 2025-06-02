8c6a974a463a3388f33a38190ee6c12d9db69b44
Sebastian McKee <sebmcke@gmail.com>
2025-06-02 15:39:44 -0500

    remove duplicate migrations

--- db/migrate/20250521195817_change_interview_questions_to_interview_question.rb
+++
@@ -1,5 +0,0 @@
-class ChangeInterviewQuestionsToInterviewQuestion < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :answer_feedbacks, :interview_questions_id, :interview_question_id
-  end
-end
